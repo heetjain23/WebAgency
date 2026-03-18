cc15e6b899afaafdc1ddf2b863585258c751b970
Heet jain <jhetu07@gmail.com>
2026-03-18 12:38:10 +0530

    Add Home page and basic UI components
    Introduce a simple page layout: add Navbar, Hero, and Footer components and a Home page that composes them. Update App to import and render the Home page. These files scaffold the initial UI structure under client/src/components and client/src/pages.

---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Navbar from '../components/ui/Navbar'
+import Hero from '../components/home/Hero'
+import Footer from '../components/ui/Footer'
+
+const Home = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <Footer />
+    </div>
+  )
+}
+
+export default Home
